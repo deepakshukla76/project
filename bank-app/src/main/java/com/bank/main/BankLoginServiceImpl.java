819125c1365e4d67739067c591ed6f989ba0d8dd
deepakshukla76 <deepakshukla1281998@gmail.com>
2021-07-16 08:35:55 +0530

    project

---
+++ bank-app/src/main/java/com/bank/main/BankLoginServiceImpl.java
@@ -0,0 +1,22 @@
+package com.bank.main;
+
+import com.bank.exception.BusinessException;
+import com.bank.model.Bank;
+import com.bank.model.Customer;
+import com.bank.service.BankLoginService;
+
+public class BankLoginServiceImpl implements BankLoginService {
+
+	@Override
+	public Bank employeeLogin(Bank bank) throws BusinessException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Customer customerLogin(Customer customer) throws BusinessException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
